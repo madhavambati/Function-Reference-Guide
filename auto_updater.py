b25cca103b287ce17aab0f1b849fe9b1f44c0703
madhavambati <peermax@iitk.ac.in>
2020-07-27 20:34:19 +0530

    auto_updater

--- auto_updater.py
+++ auto_updater.py
@@ -7,7 +7,48 @@ import pandas as pd
 # This script is for updaing main Db, runs everyday at 12:00am  
 # This checks main db files with updated db files, if there are any updates, updated files are replaced with main db files
 
-def auto_updater():
+
+
+def scheduler():
+	temp_file = "new_database/temp.txt"
+	tmp = open(temp_file, 'a+')
+	x = datetime.today().day
+	tmp.seek(0)
+	data = tmp.readlines()
+	
+	if len(data) == 0:
+		updater()
+		tmp.write("Database last updated on:"+str(x)+"\n")
+		print("*** Database Updated ***")
+	else:
+		lastline = data[-1].split(":")[-1]
+		day = int(lastline)
+
+		if day == x:
+			print("*** Database already updated for today ***")
+		if day < x:
+			updater()
+			tmp.write("Database last updated on:"+str(x)+"\n")
+			print("*** Database Updated ***")
+	tmp.close()
+	x = datetime.today()
+	y = x.replace(day=x.day, hour=0, minute=1, second=1, microsecond=0)
+
+	delta_t = y-x
+
+
+	secs = delta_t.total_seconds()
+
+	if secs < 0:
+		secs += timedelta(days=1).total_seconds()
+
+	display = str(timedelta(seconds = secs)).split(".")[0]
+	t = Timer(secs, scheduler)
+	print("next update on "+ (display) +"\n")
+	t.start()
+	
+
+def updater():
 	src = "updated/"
 	dst = "new_database/"
 	logs_file = "logs.txt"
@@ -56,25 +97,8 @@ def auto_updater():
 			else:
 				copyfile(src_path, dst_path)
 				file.write(str(datetime.now()) + " Updates in "+ path + "\n")
-
 	file.close()
-	x = datetime.today()
-	y = x.replace(day=x.day, hour=0, minute=0, second=1, microsecond=0)
 
-	delta_t = y-x
-
-	secs = delta_t.total_seconds()
-
-	if secs < 0:
-		secs += timedelta(days=1).total_seconds()
-
-	display = str(timedelta(seconds = secs)).split(".")[0]
 	
 
-	t = Timer(secs, auto_updater)
-	print(t)
-	print("*** Database Updated ***")
-	print("next update in "+ (display) +"\n")
-	t.start()
-
-auto_updater()
+scheduler()
